a21661ff8765b55a812379be62a68680e1e94a69
Anik <anikkumarb@gmail.com>
2026-06-16 23:48:19 +0530

    Configure Flask to run on 0.0.0.0 for LAN availability

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def release_notes_api():
         return jsonify(result), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
